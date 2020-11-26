fe65359f63d0b0589659b7ba40d1391faf583e1f
SujataTummala109 <71897878+SujataTummala109@users.noreply.github.com>
2020-11-26 08:05:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var fixedRect, movingRect;
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(200,100,50,80);
+  movingRect = createSprite(400, 200, 80, 30);
+  
+  fixedRect.debug = "true";
+  movingRect.debug = "true";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
+    && fixedRect.x - movingRect.x  < fixedRect.width/2 + movingRect.width/2)
+  {
+    fixedRect.shapeColor = "red";
+  movingRect.shapeColor ="red";
+  }
+  else{
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor ="green";
+  }
+  drawSprites();
+}
